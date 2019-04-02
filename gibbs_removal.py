558d3b62055fb1b5f43841f4f86af314e3adccd1
Rafael Neto Henriques <rafaelnh21@gmail.com>
2019-04-02 11:57:51 +0100

    Update Gibbs_removal.py
    Correct version of the code

--- gibbs_removal.py
+++ gibbs_removal.py
@@ -9,72 +9,34 @@
 #
 # Kellner E, Dhital B, Kiselev VG, Reisert M. Gibbs-ringing artifact removal
 # based on local subvoxel-shifts. Magn Reson Med. 2015 doi: 10.1002/mrm.26054.
+#
+# Full adaption of the code is described in chapter 3 of thesis:
+# 
+# Neto Henriques, R., 2018. Advanced Methods for Diffusion MRI Data
+# Analysis and their Application to the Healthy Ageing Brain
+# (Doctoral thesis). https://doi.org/10.17863/CAM.29356
 # -------------------------------------------------------------------------
 
 import numpy as np
 
-
-def image_shift(x, s, a=0):
-    """ Shift elements of matrix x by a position difference s along axis a.
-    
-    Parameters
-    ----------
-    x : 2D ndarray
-        Original values of matrix x
-    s : float
-        Value of the shift
-    a : int (0 or 1)
-        Axis along which the shift will be applied.
-        Default a is set to 0.
-    
-    Returns
-    -------
-    Shifted version of matrix x.
-    
-    Note
-    ----
-    The values of the new shifted matrix are calculated using linear
-    interpolation of the values of the original matrix.
-    """
-    if a:
-        xs = x.copy()
-    else:
-        xs = x.T.copy()
-
-    if s >= 1 or s <=-1:
-        raise ValueError('Shift should be a value between -1 and 1')
-    elif s > 0:
-        xs[:, :-1] = (xs[:, 1:] - xs[:, :-1]) * s + xs[:, :-1]   
-    else:
-        xs[:, 1:] = (xs[:, 1:] - xs[:, :-1]) * (1+s) + xs[:, :-1]
-    
-    if a:
-        return xs
-    else:
-        return xs.T
-
-
-def image_tv(x, fn=0, nn=3, a=0):
+def _image_tv(x, axis=0, n_points=3):
     """ Computes total variation (TV) of matrix x along axis a in two
-    directions.
-    
+    directions
+
     Parameters
     ----------
     x : 2D ndarray
-        matrix x 
-    fn : int
-        Distance of first neighbor to be included in TV calculation. If fn=0
-        the own point is also included in the TV calculation.
-    nn : int
-        Number of points to be included in TV calculation.
-    a : int (0 or 1)
+        matrix x
+    axis : int (0 or 1)
         Axis along which TV will be calculated. Default a is set to 0.
-    
+    n_points : int
+        Number of points to be included in TV calculation.
+
     Returns
     -------
-    PTV : 2D ndarray
+    ptv : 2D ndarray
         Total variation calculated from the right neighbors of each point
-    NTV : 2D ndarray
+    ntv : 2D ndarray
         Total variation calculated from the left neighbors of each point
 
     Note
@@ -82,128 +44,138 @@ def image_tv(x, fn=0, nn=3, a=0):
     This function was created to deal with gibbs artefacts of MR images.
     Assuming that MR images are reconstructed from estimates of their Fourier
     expansion coefficients, during TV calculation matrix x can taken as and
-    periodic signal. In this way NTV values on the image left boundary is
-    computed using the time series values on the right boundary and vice versa.
+    periodic signal. In this way ntv values on the image left boundary is
+    computed using the time series values on the right boundary and ptv values
+    are computed using the time series values on the left boundary.
     """
-    if a:
+    if axis:
         xs = x.copy()
     else:
         xs = x.T.copy()
 
-    xs = np.concatenate((xs[:, (-nn-fn):], xs, xs[:, 0:(nn+fn)]), axis=1)
+    # Add copies of the data so that data extreme points are also analysed
+    xs = np.concatenate((xs[:, (-n_points-1):], xs, xs[:, 0:(n_points+1)]),
+                        axis=1)
 
-    PTV = np.absolute(xs[:, (nn+fn):(-nn-fn)] - xs[:, (nn+fn+1):(-nn-fn+1)])
-    NTV = np.absolute(xs[:, (nn+fn):(-nn-fn)] - xs[:, (nn+fn-1):(-nn-fn-1)])
-    for n in np.linspace(fn+1, nn-2, num=nn-2, dtype=int):
-        PTV = PTV + np.absolute(xs[:, (nn+fn+n):(-nn-fn+n)] - \
-                                xs[:, (nn+fn+n+1):(-nn-fn+n+1)])
-        NTV = NTV + np.absolute(xs[:, (nn+fn-n):(-nn-fn-n)] - \
-                                xs[:, (nn+fn-n-1):(-nn-fn-n-1)])
-    
-    if a:
-        return PTV, NTV
+    ptv = np.absolute(xs[:, (n_points+1):(-n_points-1)] -
+                      xs[:, (n_points+2):(-n_points)])
+    ntv = np.absolute(xs[:, (n_points+1):(-n_points-1)] -
+                      xs[:, (n_points):(-n_points-2)])
+    for n in range(1, n_points):
+        ptv = ptv + np.absolute(xs[:, (n_points+1+n):(-n_points-1+n)] -
+                                xs[:, (n_points+2+n):(-n_points+n)])
+        ntv = ntv + np.absolute(xs[:, (n_points+1-n):(-n_points-1-n)] -
+                                xs[:, (n_points-n):(-n_points-2-n)])
+
+    if axis:
+        return ptv, ntv
     else:
-        return PTV.T, NTV.T
+        return ptv.T, ntv.T
 
 
-def gibbs_removal_1d(x, a=0, fn=0, nn=3):
-    """ Decreases gibbs ringing along axis a.
-    
+def _gibbs_removal_1d(x, axis=0, n_points=3):
+    """ Decreases gibbs ringing along an axis 'a' using fourier sub-shifts
+
     Parameters
     ----------
     x : 2D ndarray
         Matrix x.
-    a : int (0 or 1)
-        Axis along which gibbs oscilations will be reduced. Default a is set
+    axis : int (0 or 1)
+        Axis along which gibbs oscillations will be reduced. Default a is set
         to 0 (i.e. gibbs are reduce along axis y).
-    fn : int, optional
-        Distance of first neighbour used to access local TV (see note).
-        Default is set to 0 which means that the own point is also used to
-        access local TV.
-    nn : int, optional
+    n_points : int, optional
         Number of neighbour points to access local TV (see note). Default is
         set to 3.
-    
+   
     Returns
     -------
     xc : 2D ndarray
-        Matrix with gibbs oscilantions reduced along axis a.
-    tv : 2D ndarray
-        Global TV which show variation not removed (edges, anatomical
-        variation, non-oscilatory component of gibbs artefact normally present
-        in image background, etc.)
-
+        Matrix with gibbs oscillations reduced along an axis 'a'.
+    
     Note
     ----
-    This function decreases the effects of gibbs oscilations based on the
+    This function decreases the effects of gibbs oscillations based on the
     analysis of local total variation (TV). Although artefact correction is
     done based on two adjanced points for each voxel, total variation should be
     accessed in a larger range of neigbors. If you want to adjust the number
-    and index of the neigbors to be considered in TV calculation please change
-    parameters nn and fn.
+    of the neigbors to be considered in TV calculation please change
+    parameter nn.
     """
     ssamp = np.linspace(0.02, 0.9, num=45)
 
+    if axis:
+        xs = x.copy()
+    else:
+        xs = x.T.copy()
+	
     # TV for shift zero (baseline)
-    TVR, TVL = image_tv(x, fn=fn, nn=nn, a=a)
-    TVP = np.minimum(TVR, TVL)
-    TVN = TVP.copy()
-    
+    tvr, tvl = _image_tv(xs, axis=1, n_points=n_points)
+    tvp = np.minimum(tvr, tvl)
+    tvn = tvp.copy()
+
     # Find optimal shift for gibbs removal
-    ISP = x.copy()
-    ISN = x.copy()
-    SP = np.zeros(x.shape)
-    SN = np.zeros(x.shape)
+    isp = xs.copy()
+    isn = xs.copy()
+    sp = np.zeros(xs.shape)
+    sn = np.zeros(xs.shape)
+    N = xs.shape[1]
+    c = np.fft.fftshift(np.fft.fft2(xs))
+    k = np.linspace(-N/2, N/2-1, num=N)
+    k = (2.0j * np.pi * k) / N
     for s in ssamp:
-        # Image shift using current pos shift
-        Img_p = image_shift(x, s, a=a)
-        TVSR, TVSL = image_tv(Img_p, fn=fn, nn=nn, a=a)
-        TVS_p = np.minimum(TVSR, TVSL)
-        # Image shift using current neg shift
-        Img_n = image_shift(x, -s, a=a)
-        TVSR, TVSL = image_tv(Img_n, fn=fn, nn=nn, a=a)
-        TVS_n = np.minimum(TVSR, TVSL)
+        # Access positive shift for given s
+        img_p = abs(np.fft.ifft2(np.fft.fftshift(c * np.exp(k*s))))
+        tvsr, tvsl = _image_tv(img_p, axis=1, n_points=n_points)
+        tvs_p = np.minimum(tvsr, tvsl)
+
+        # Access negative shift for given s
+        img_n = abs(np.fft.ifft2(np.fft.fftshift(c * np.exp(-k*s))))
+        tvsr, tvsl = _image_tv(img_n, axis=1, n_points=n_points)
+        tvs_n = np.minimum(tvsr, tvsl)
+
         # Update positive shift params
-        ISP[TVP > TVS_p] = Img_p[TVP > TVS_p]
-        SP[TVP > TVS_p] = s
-        TVP[TVP > TVS_p] = TVS_p[TVP > TVS_p]
+        isp[tvp > tvs_p] = img_p[tvp > tvs_p]
+        sp[tvp > tvs_p] = s
+        tvp[tvp > tvs_p] = tvs_p[tvp > tvs_p]
+
         # Update negative shift params
-        ISN[TVN > TVS_n] = Img_n[TVN > TVS_n]
-        SN[TVN > TVS_n] = s
-        TVN[TVN > TVS_n] = TVS_n[TVN > TVS_n]
+        isn[tvn > tvs_n] = img_n[tvn > tvs_n]
+        sn[tvn > tvs_n] = s
+        tvn[tvn > tvs_n] = tvs_n[tvn > tvs_n]
 
-    # apply correction if SP and SN are not zeros
-    xc = x.copy()
-    idx = np.nonzero(SP + SN)
-    xc[idx] = (ISP[idx] - ISN[idx])/(SP[idx] + SN[idx])*SN[idx] + ISN[idx]
+    # check non-zero sub-voxel shifts
+    idx = np.nonzero(sp + sn)
+	
+	# use positive and negative optimal sub-voxel shifts to interpolate to
+    # original grid points
+    xs[idx] = (isp[idx] - isn[idx])/(sp[idx] + sn[idx])*sn[idx] + isn[idx]
 
-    # Global minimum TV (can be useful as edge detector)
-    tv = np.minimum(TVN, TVP)
+    if axis:
+        return xs
+    else:
+        return xs.T
 
-    return xc, tv
 
-
-def gibbs_removal_2d_weigthing_functions(shape):
+def _weights(shape):
     """ Computes the weights necessary to combine two images processed by
     the 1D gibbs removal procedure along two different axis [1]_.
-    
+
     Parameters
     ----------
     shape : tuple
         shape of the image
 
     Returns
-    -------            
+    -------
     G0 : 2D ndarray
         Weights for the image corrected along axis 0.
     G1 : 2D ndarray
         Weights for the image corrected along axis 1.
 
-
     References
     ----------
     .. [1] Kellner E, Dhital B, Kiselev VG, Reisert M. Gibbs-ringing artifact
-           removal based on local subvoxel-shifts. Magn Reson Med. 2015
+           removal based on local subvoxel-shifts. Magn Reson Med. 2016
            doi: 10.1002/mrm.26054.
     """
     G0 = np.zeros(shape)
@@ -212,7 +184,7 @@ def gibbs_removal_2d_weigthing_functions(shape):
 
     # Middle points
     K1, K0 = np.meshgrid(k[1:-1], k[1:-1])
-    cosk0 = 1.0 + np.cos(K0) 
+    cosk0 = 1.0 + np.cos(K0)
     cosk1 = 1.0 + np.cos(K1)
     G1[1:-1, 1:-1] = cosk0 / (cosk0+cosk1)
     G0[1:-1, 1:-1] = cosk1 / (cosk0+cosk1)
@@ -226,125 +198,138 @@ def gibbs_removal_2d_weigthing_functions(shape):
     return G0, G1
 
 
-def gibbs_removal_2d(image, fn=0, nn=3, G0=None, G1=None):
+def _gibbs_removal_2d(image, n_points=3, G0=None, G1=None):
     """ Decreases gibbs ringing of a 2D image.
-    
+
     Parameters
     ----------
     image : 2D ndarray
         Matrix cotaining the 2D image.
-    fn : int, optional
-        Distance of first neighbour used to access local TV (see note).
-        Default is set to 0 which means that the own point is also used to
-        access local TV.
-    nn : int, optional
+    n_points : int, optional
         Number of neighbour points to access local TV (see note). Default is
         set to 3.
     G0 : 2D ndarray, optional.
         Weights for the image corrected along axis 1. If not given, the
-        function estimates them using function:
-            gibbs_removal_2d_weigthing_functions
+        function estimates them using function :func:`_weights`
     G1 : 2D ndarray
         Weights for the image corrected along axis 1. If not given, the
-        function estimates them using function:
-            gibbs_removal_2d_weigthing_functions
-    
+        function estimates them using function :func:`_weights`
+
     Returns
     -------
     imagec : 2D ndarray
-        Matrix with gibbs oscilantions reduced along axis a.
+        Matrix with gibbs oscillations reduced along axis a.
     tv : 2D ndarray
         Global TV which show variation not removed by the algorithm (edges,
-        anatomical variation, non-oscilatory component of gibbs artefact
+        anatomical variation, non-oscillatory component of gibbs artefact
         normally present in image background, etc.)
 
     Note
     ----
-    This function decreases the effects of gibbs oscilations based on the
+    This function decreases the effects of gibbs oscillations based on the
     analysis of local total variation (TV) along the two axis of the image.
     Although artefact correction is done based on each point primary adjanced
     neighbors, total variation should be accessed in a larger range of
-    neigbors. If you want to adjust the number and index of the neigbors to be
-    considered in TV calculation please change parameters nn and fn.
+    neighbors. If you want to adjust the number of the neigbors to be
+    considered in TV calculation please change parameter nn.
+    
+    References
+    ----------
+    Please cite the following articles
+    .. [1] Neto Henriques, R., 2018. Advanced Methods for Diffusion MRI Data
+           Analysis and their Application to the Healthy Ageing Brain
+          (Doctoral thesis). https://doi.org/10.17863/CAM.29356
+    .. [2] Kellner E, Dhital B, Kiselev VG, Reisert M. Gibbs-ringing artifact
+           removal based on local subvoxel-shifts. Magn Reson Med. 2016
+           doi: 10.1002/mrm.26054.
     """
-    if np.any(G0) == None or np.any(G1) == None:
-        G0, G1 = gibbs_removal_2d_weigthing_functions(image.shape)
+    if np.any(G0) is None or np.any(G1) is None:
+        G0, G1 = _weights(image.shape)
 
-    img_c1, tv_c1 =  gibbs_removal_1d(image, a=1, fn=fn, nn=nn)
-    img_c0, tv_c0 =  gibbs_removal_1d(image, a=0, fn=fn, nn=nn)
+    img_c1 = _gibbs_removal_1d(image, axis=1, n_points=n_points)
+    img_c0 = _gibbs_removal_1d(image, axis=0, n_points=n_points)
 
     C1 = np.fft.fft2(img_c1)
     C0 = np.fft.fft2(img_c0)
     imagec = abs(np.fft.ifft2(np.fft.fftshift(C1)*G1 + np.fft.fftshift(C0)*G0))
 
-    # Just to access performance
-    T1 = np.fft.fft2(tv_c1)
-    T0 = np.fft.fft2(tv_c0)
-    tv = abs(np.fft.ifft2(np.fft.fftshift(T1)*G1 + np.fft.fftshift(T0)*G0))
-
-    return imagec, tv
+    return imagec
 
 
-def volume_gibbs_removal(vol, fn=0, nn=3):
-    """ Decreases gibbs ringing of image's volumes.
-    
+def gibbs_removal(vol, slice_axis=2, n_points=3):
+    """ Suppresses gibbs ringing artefacts of images volumes.
+
     Parameters
     ----------
-    vol : array ([X, Y, Z]) or ([X, Y, Z, g])
+    vol : ndarray ([X, Y]), ([X, Y, Z]) or ([X, Y, Z, g])
         Matrix containing one volume (3D) or multiple (4D) volumes of images.
-    fn : int, optional
-        Distance of first neighbour used to access local TV (see note).
-        Default is set to 0 which means that the own point is also used to
-        access local TV.
-    nn : int, optional
+    slice_axis : int (0, 1, or 2)
+        Data axis corresponding to the number of acquired slices. Default is
+	    set to the third axis
+    n_points : int, optional
         Number of neighbour points to access local TV (see note). Default is
         set to 3.
-    
+
     Returns
     -------
-    vol : array ([X, Y, Z]) or ([X, Y, Z, g])
+    vol : ndarray ([X, Y]), ([X, Y, Z]) or ([X, Y, Z, g])
         Matrix containing one volume (3D) or multiple (4D) volumes of corrected
         images.
-    tv : array ([X, Y, Z]) or ([X, Y, Z, g])
-        Global TV which show variation not removed by the algorithm (edges,
-        anatomical variation, non-oscilatory component of gibbs artefact
-        normally present in image background, etc.)
 
     Notes
     -----
-    1) This function correct gibbs artifacts assuming that images were acquired
-       from 2D k-space Fourier expansion coefficients. Based on this,
-       correction is performed by applying the 2D gibbs artefact revomal slice
-       by slice. Please insure that original images were acquired along the
-       z-axis. If this is not the case, you have to orient your dataset in the
-       way that axis 2 of matrix vol correspond to different acquired images.
-       For 4D matrix last element should always correspond to different sets
-       of image volumes.
-    2) This function decreases the effects of gibbs oscilations based on the
-       analysis of local total variation (TV) along the two first axis of the
-       volumes. Although artefact correction is done based on each point
-       primary adjanced neighbors, total variation should be accessed in a
-       larger range of neigbors. If you want to adjust the number and index of
-       the neigbors to be considered in TV calculation please change parameters
-       nn and fn.
+    For 4D matrix last element should always correspond to the number of
+    diffusion gradient directions.
+
+    References
+    ----------
+    Please cite the following articles
+    .. [1] Neto Henriques, R., 2018. Advanced Methods for Diffusion MRI Data
+           Analysis and their Application to the Healthy Ageing Brain
+           (Doctoral thesis). https://doi.org/10.17863/CAM.29356
+    .. [2] Kellner E, Dhital B, Kiselev VG, Reisert M. Gibbs-ringing artifact
+           removal based on local subvoxel-shifts. Magn Reson Med. 2016
+           doi: 10.1002/mrm.26054.
     """
     nd = vol.ndim
+
+    # check the axis corresponding to different slices
+    # 1) This axis cannot be larger than 2
+    if slice_axis > 2:
+        raise ValueError("Different slices have to be organized along" +
+                         "one of the 3 first matrix dimensions")
+
+    # 2) If this is not 2, swap axis so that different slices are ordered
+    # along axis 2. Note that swapping is not required if data is already a
+    # single image
+    elif slice_axis < 2 and nd > 2:
+        vol = np.swapaxes(vol, slice_axis, 2)
+           
+    # check matrix dimension
     if nd == 4:
         inishap = vol.shape
         vol = vol.reshape((inishap[0], inishap[1], inishap[2]*inishap[3]))
-         
-    shap = vol.shape
-    G0, G1 = gibbs_removal_2d_weigthing_functions(shap[:2])
+    elif nd > 4:
+        raise ValueError("Data have to be a 4D, 3D or 2D matrix")
+    elif nd < 2:
+        raise ValueError("Data is not an image")
 
-    tv = np.empty(shap)
-    for i in range(shap[2]):
-        imgc, tvi = gibbs_removal_2d(vol[:, :, i], fn=fn, nn=nn, G0=G0, G1=G1)
-        vol[:, :, i] = imgc
-        tv[:, :, i] = tvi
-    
+    # Produce weigthing functions for 2D gibbs removal
+    shap = vol.shape
+    G0, G1 = _weights(shap[:2])
+
+    # Run gibbs removal of 2D images
+    if nd == 2:
+        vol = _gibbs_removal_2d(vol, n_points=n_points, G0=G0, G1=G1)
+    else:
+        for vi in range(shap[2]):
+            vol[:, :, vi] = _gibbs_removal_2d(vol[:, :, vi], n_points=n_points,
+                                              G0=G0, G1=G1)
+
+    # Reshape data to original format
     if nd == 4:
         vol = vol.reshape(inishap)
-        tv = tv.reshape(inishap)
+    if slice_axis < 2 and nd > 2:
+        vol = np.swapaxes(vol, slice_axis, 2)
 
-    return vol, tv
-
+    return vol
